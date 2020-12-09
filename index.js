f9fdf15367068bd8466078eae1b3597af072be59
Jeffrey (Jeff) Straney <straney.jeffrey@ocls.info>
2020-12-09 11:14:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  useArgs: require('./lib/useArgs'),
+  consumeArgs: require('./lib/consumeArgs'),
+  viewTemplate: require('./lib/viewTemplate'),
+  viewJSON: require('./lib/viewJSON'),
+  requireAuth: require('./lib/requireAuth'),
+};
